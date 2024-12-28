464fe64c034b2ddd7240f13007092fbda70e44b7
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-28 20:59:36 +0700

    feat(home): University list spacing

--- app/src/main/kotlin/com/aldyaz/univuniv/ui/home/HomeUniversityItem.kt
+++ app/src/main/kotlin/com/aldyaz/univuniv/ui/home/HomeUniversityItem.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aldyaz.univuniv.presentation.model.UniversityPresentationModel
@@ -32,7 +33,8 @@ fun HomeUniversityItem(
                 content = {
                     Text(
                         text = item.name,
-                        style = MaterialTheme.typography.titleMedium
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.Bold
                     )
                     Spacer(modifier = Modifier.padding(vertical = 4.dp))
                     Text(
