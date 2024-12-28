2cb5ca2cd43f33d42abdc665e2f251b0d7f75b21
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-28 21:18:40 +0700

    feat(home): Home app bar fix invalid action button invocation

--- app/src/main/kotlin/com/aldyaz/univuniv/ui/home/HomeAppBar.kt
+++ app/src/main/kotlin/com/aldyaz/univuniv/ui/home/HomeAppBar.kt
@@ -25,7 +25,7 @@ fun HomeAppBar(
                 style = MaterialTheme.typography.titleLarge
             )
         },
-        navigationIcon = {
+        actions = {
             IconButton(
                 onClick = onClickSearch,
                 content = {
