2b8c6a65a65709301dfd36506bab4c75192faa07
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-29 23:02:26 +0700

    feat(home): GetUniversitiesUseCase fix unconsumed test events

--- app/src/test/kotlin/com/aldyaz/univuniv/domain/interactor/GetUniversitiesUseCaseTest.kt
+++ app/src/test/kotlin/com/aldyaz/univuniv/domain/interactor/GetUniversitiesUseCaseTest.kt
@@ -2,7 +2,7 @@ package com.aldyaz.univuniv.domain.interactor
 
 import app.cash.turbine.test
 import com.aldyaz.univuniv.data.repository.FakeUniversityRepository
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -19,11 +19,17 @@ class GetUniversitiesUseCaseTest {
     }
 
     @Test
-    fun `get universities, should return certain list`() = runBlocking {
+    fun `get universities, should return certain list`() = runTest {
         useCase(Unit).test {
+            val repoFlow = repository.getUniversities().testIn(this)
             val expected = repository.dummyList()
+            repoFlow.awaitItem()
+            repoFlow.awaitComplete()
             val actual = awaitItem()
+            awaitComplete()
+            Assertions.assertEquals(expected.size, actual.size)
             Assertions.assertEquals(expected.first(), actual.first())
+            Assertions.assertEquals(expected.first().name, actual.first().name)
         }
     }
 }
