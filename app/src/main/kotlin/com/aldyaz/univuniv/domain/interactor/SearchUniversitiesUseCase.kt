02109aede56f9c662e1bac2d84c64891bf86958f
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-29 09:58:50 +0700

    feat(search): Create search university use case

---
+++ app/src/main/kotlin/com/aldyaz/univuniv/domain/interactor/SearchUniversitiesUseCase.kt
@@ -0,0 +1,15 @@
+package com.aldyaz.univuniv.domain.interactor
+
+import com.aldyaz.univuniv.core.domain.FlowUseCase
+import com.aldyaz.univuniv.domain.model.UniversityDomainModel
+import com.aldyaz.univuniv.domain.repository.UniversityRepository
+import kotlinx.coroutines.flow.Flow
+
+class SearchUniversitiesUseCase(
+    private val repository: UniversityRepository
+) : FlowUseCase<String, List<UniversityDomainModel>>() {
+
+    override fun execute(param: String): Flow<List<UniversityDomainModel>> {
+        return repository.getUniversitiesByName(param)
+    }
+}
