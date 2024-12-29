d7a87f87fff05eb190a30b9b4381d0e7256784c8
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-29 15:06:40 +0700

    feat(search): Add loading and error state

--- app/src/main/kotlin/com/aldyaz/univuniv/presentation/state/SearchState.kt
+++ app/src/main/kotlin/com/aldyaz/univuniv/presentation/state/SearchState.kt
@@ -1,7 +1,18 @@
 package com.aldyaz.univuniv.presentation.state
 
+import com.aldyaz.univuniv.core.presentation.ErrorPresentationModel
 import com.aldyaz.univuniv.presentation.model.UniversityPresentationModel
 
 data class SearchState(
+    val loading: Boolean = true,
+    val error: ErrorPresentationModel? = null,
     val data: List<UniversityPresentationModel> = emptyList()
-)
+) {
+
+    val isError = error != null
+    val isSuccess = !loading && error == null
+
+    companion object {
+        val Initial = SearchState()
+    }
+}
