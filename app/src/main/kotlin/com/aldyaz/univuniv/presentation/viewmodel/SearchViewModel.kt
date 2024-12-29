ab7658b7ffff68caa4ad8719b704f8d119a6576f
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-29 14:52:22 +0700

    feat(search): UI cosmetics, fixed up search query process

--- app/src/main/kotlin/com/aldyaz/univuniv/presentation/viewmodel/SearchViewModel.kt
+++ app/src/main/kotlin/com/aldyaz/univuniv/presentation/viewmodel/SearchViewModel.kt
@@ -7,13 +7,10 @@ import com.aldyaz.univuniv.presentation.intent.SearchIntent
 import com.aldyaz.univuniv.presentation.mapper.UniversityToPresentationMapper
 import com.aldyaz.univuniv.presentation.state.SearchState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,39 +19,25 @@ class SearchViewModel @Inject constructor(
     private val universityToPresentationMapper: UniversityToPresentationMapper
 ) : BaseViewModel<SearchIntent>() {
 
-    private val _searchQuery = MutableStateFlow("")
-
     private val _state = MutableStateFlow(SearchState())
-    val state = combine(
-        _state,
-        fetchSearch()
-    ) { state, items ->
-        var newState = state
-        newState = newState.copy(
-            data = List(items.size) {
-                universityToPresentationMapper(items[it])
-            }
-        )
-        newState
-    }.stateIn(
-        scope = viewModelScope,
-        started = SharingStarted.WhileSubscribed(5000),
-        initialValue = SearchState()
-    )
+    val state = _state.asStateFlow()
 
     override fun onIntent(intent: SearchIntent) {
         when (intent) {
-            is SearchIntent.Fetch -> {
-                _searchQuery.value = intent.query
-            }
+            is SearchIntent.Fetch -> getUniversities(intent.query)
         }
     }
 
-    private fun fetchSearch() = _searchQuery
-        .debounce(300)
-        .flatMapLatest {
-            if (it.isNotEmpty() || it.length > 1) searchUniversitiesUseCase(it) else flowOf(
-                emptyList()
-            )
-        }
+    private fun getUniversities(query: String) = viewModelScope.launch(Dispatchers.IO) {
+        searchUniversitiesUseCase(query)
+            .collect { items ->
+                _state.updateState {
+                    copy(
+                        data = List(items.size) {
+                            universityToPresentationMapper(items[it])
+                        }
+                    )
+                }
+            }
+    }
 }
