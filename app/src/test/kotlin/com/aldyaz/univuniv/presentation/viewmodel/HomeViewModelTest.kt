ed6cbdf6cbb2a1285aa866b633eb8bedd7c74342
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-30 10:35:21 +0700

    feat(home): Add test case for success and error data

--- app/src/test/kotlin/com/aldyaz/univuniv/presentation/viewmodel/HomeViewModelTest.kt
+++ app/src/test/kotlin/com/aldyaz/univuniv/presentation/viewmodel/HomeViewModelTest.kt
@@ -1,15 +1,20 @@
 package com.aldyaz.univuniv.presentation.viewmodel
 
 import app.cash.turbine.test
+import com.aldyaz.univuniv.core.domain.exception.CloudApiException
+import com.aldyaz.univuniv.core.domain.exception.UnknownException
+import com.aldyaz.univuniv.core.presentation.ErrorPresentationModel
 import com.aldyaz.univuniv.core.presentation.ExceptionToPresentationMapper
 import com.aldyaz.univuniv.domain.interactor.GetUniversitiesUseCase
 import com.aldyaz.univuniv.domain.model.UniversityDomainModel
+import com.aldyaz.univuniv.presentation.intent.HomeIntent
 import com.aldyaz.univuniv.presentation.mapper.UniversitiesToPresentationMapper
 import com.aldyaz.univuniv.presentation.model.UniversityPresentationModel
 import com.aldyaz.univuniv.presentation.state.HomeState
 import com.aldyaz.univuniv.utils.CoroutineTestExtension
-import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions
@@ -24,6 +29,7 @@ class HomeViewModelTest {
     private val universitiesToPresentationMapper: UniversitiesToPresentationMapper =
         mockk(relaxed = true)
     private val exceptionToPresentationMapper: ExceptionToPresentationMapper = mockk(relaxed = true)
+
     private lateinit var viewModel: HomeViewModel
 
     @BeforeEach
@@ -40,6 +46,71 @@ class HomeViewModelTest {
         viewModel.state.test {
             val initialState = awaitItem()
             Assertions.assertEquals(HomeState.Initial, initialState)
+        }
+    }
+
+    @Test
+    fun `invoke fetch universities should return university list`() = runTest {
+        viewModel.state.test {
+            val expected = dummyPresentationList()
+
+            awaitItem()
+
+            every { getUniversitiesUseCase(Unit) }.returns(flowOf(dummyDomainList()))
+            every { universitiesToPresentationMapper(dummyDomainList()) }.returns(expected)
+
+            viewModel.onIntent(HomeIntent.Fetch)
+
+            val resultState = awaitItem()
+            Assertions.assertEquals(expected.size, resultState.data.size)
+        }
+    }
+
+    @Test
+    fun `invoke fetch universities should return cloud api error`() = runTest {
+        viewModel.state.test {
+            val errMessage = "api error!"
+            val exception = CloudApiException(errMessage)
+            val expected = ErrorPresentationModel.CloudApiError(errMessage)
+
+            awaitItem()
+
+            every { getUniversitiesUseCase(Unit) }.returns(
+                flow {
+                    throw exception
+                }
+            )
+            every { exceptionToPresentationMapper(exception) } returns expected
+
+            viewModel.onIntent(HomeIntent.Fetch)
+
+            val resultState = awaitItem()
+            Assertions.assertEquals(expected, resultState.error)
+            Assertions.assertEquals(expected.message, resultState.error?.errorMessage)
+        }
+    }
+
+    @Test
+    fun `invoke fetch universities should return unknown error`() = runTest {
+        viewModel.state.test {
+            val errMessage = "unknown error!"
+            val exception = UnknownException(errMessage)
+            val expected = ErrorPresentationModel.Unknown(errMessage)
+
+            awaitItem()
+
+            every { getUniversitiesUseCase(Unit) }.returns(
+                flow {
+                    throw exception
+                }
+            )
+            every { exceptionToPresentationMapper(exception) } returns expected
+
+            viewModel.onIntent(HomeIntent.Fetch)
+
+            val resultState = awaitItem()
+            Assertions.assertEquals(expected, resultState.error)
+            Assertions.assertEquals(expected.message, resultState.error?.errorMessage)
         }
     }
 
