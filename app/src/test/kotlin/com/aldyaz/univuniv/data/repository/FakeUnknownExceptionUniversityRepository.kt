4800158ae5703136418259e76cf0c376c151b299
Aldyaz Nugroho <aldyaz14@gmail.com>
2024-12-30 12:08:11 +0700

    feat(search): SearchUniversitiesUseCase add test case for exception

---
+++ app/src/test/kotlin/com/aldyaz/univuniv/data/repository/FakeUnknownExceptionUniversityRepository.kt
@@ -0,0 +1,17 @@
+package com.aldyaz.univuniv.data.repository
+
+import com.aldyaz.univuniv.core.domain.exception.UnknownException
+import com.aldyaz.univuniv.domain.model.UniversityDomainModel
+import com.aldyaz.univuniv.domain.repository.UniversityRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+
+class FakeUnknownExceptionUniversityRepository : UniversityRepository {
+
+    override fun getUniversities(): Flow<List<UniversityDomainModel>> = flowOf(emptyList())
+
+    override fun searchUniversities(query: String): Flow<List<UniversityDomainModel>> = flow {
+        throw UnknownException()
+    }
+}
--- app/src/test/kotlin/com/aldyaz/univuniv/domain/interactor/SearchUniversitiesUseCaseTest.kt
+++ app/src/test/kotlin/com/aldyaz/univuniv/domain/interactor/SearchUniversitiesUseCaseTest.kt
@@ -1,7 +1,11 @@
 package com.aldyaz.univuniv.domain.interactor
 
 import app.cash.turbine.test
+import com.aldyaz.univuniv.core.domain.exception.CloudApiException
+import com.aldyaz.univuniv.core.domain.exception.UnknownException
+import com.aldyaz.univuniv.data.repository.FakeCloudApiExceptionUniversityRepository
 import com.aldyaz.univuniv.data.repository.FakeUniversityRepository
+import com.aldyaz.univuniv.data.repository.FakeUnknownExceptionUniversityRepository
 import com.aldyaz.univuniv.domain.model.UniversityDomainModel
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions
@@ -10,17 +14,12 @@ import org.junit.jupiter.api.Test
 
 class SearchUniversitiesUseCaseTest {
 
-    private lateinit var repository: FakeUniversityRepository
     private lateinit var useCase: SearchUniversitiesUseCase
-
-    @BeforeEach
-    fun setup() {
-        repository = FakeUniversityRepository()
-        useCase = SearchUniversitiesUseCase(repository)
-    }
 
     @Test
     fun `search universities should return certain list`() = runTest {
+        val repository = FakeUniversityRepository()
+        useCase = SearchUniversitiesUseCase(repository)
         useCase("").test {
             val expected = repository.dummyList
             val actual = awaitItem()
@@ -33,6 +32,8 @@ class SearchUniversitiesUseCaseTest {
 
     @Test
     fun `search universities should return empty list`() = runTest {
+        val repository = FakeUniversityRepository()
+        useCase = SearchUniversitiesUseCase(repository)
         repository.dummyList = emptyList()
         useCase("").test {
             val expected = emptyList<UniversityDomainModel>()
@@ -40,6 +41,28 @@ class SearchUniversitiesUseCaseTest {
             awaitComplete()
             Assertions.assertTrue(actual.isEmpty())
             Assertions.assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `search universities should throw cloud api exception`() = runTest {
+        val repository = FakeCloudApiExceptionUniversityRepository()
+        useCase = SearchUniversitiesUseCase(repository)
+        useCase("").test {
+            val exception = awaitError()
+            Assertions.assertTrue(exception is CloudApiException)
+            Assertions.assertEquals("api error!", (exception as CloudApiException).errorMessage)
+        }
+    }
+
+    @Test
+    fun `search universities should throw unknown exception`() = runTest {
+        val repository = FakeUnknownExceptionUniversityRepository()
+        useCase = SearchUniversitiesUseCase(repository)
+        useCase("").test {
+            val exception = awaitError()
+            Assertions.assertTrue(exception is UnknownException)
+            Assertions.assertEquals("Unknown exception!", exception.message)
         }
     }
 }
